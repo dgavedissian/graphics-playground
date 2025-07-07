7dcc38e5f08e41e59bfb3408e147d84b4b6e4cbc
David Avedissian <git@dga.uk>
2025-07-07 12:38:00 +0100

    Implement shading, divide work into N threads.

--- cpu_raytracing/raytracing.cpp
+++ cpu_raytracing/raytracing.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <random>
+#include <thread>
+
 #include <glm.hpp>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -8,6 +11,60 @@
 using color = glm::dvec3;
 using point3 = glm::dvec3;
 using vec3 = glm::dvec3;
+
+struct interval {
+    double min;
+    double max;
+
+    interval() : min(std::numeric_limits<double>::min()), max(std::numeric_limits<double>::max()) {}
+    interval(double min_val, double max_val) : min(min_val), max(max_val) {}
+
+    double length() const {
+        return max - min;
+    }
+
+    bool contains(double value) const {
+        return value >= min && value <= max;
+    }
+    
+    bool surrounds(double value) const {
+        return value > min && value < max;
+    }
+
+    double clamp(double x) const {
+        return (x < min) ? min : (x > max) ? max : x;
+    }
+
+    static const interval empty;
+};
+
+const interval interval::empty = interval(std::numeric_limits<double>::max(), std::numeric_limits<double>::min());
+
+inline double randomDouble() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double randomDouble(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*randomDouble();
+}
+
+inline vec3 randomUnitVector() {
+    while (true) {
+        auto p = vec3{randomDouble(-1, 1), randomDouble(-1, 1), randomDouble(-1, 1)};
+        auto lengthSq = glm::dot(p, p);
+        if (1e-160 < lengthSq && lengthSq <= 1) {
+            return p / sqrt(lengthSq);
+        }
+    }
+}
+
+inline vec3 randomOnHemisphere(const vec3& normal) {
+    vec3 onUnitSphere = randomUnitVector();
+    return dot(onUnitSphere, normal) < 0.0 ? -onUnitSphere : onUnitSphere;
+}
 
 class Ray {
 public:
@@ -41,14 +98,14 @@ class Hittable {
 public:
     virtual ~Hittable() = default;
 
-    virtual bool hit(const Ray& r, double tmin, double tmax, HitResult& result) const = 0;
+    virtual bool hit(const Ray& r, interval t, HitResult& result) const = 0;
 };
 
 class Sphere : public Hittable {
 public:
     Sphere(const point3& centre, double radius) : centre_(centre), radius_(std::fmax(0.0, radius)) {}
 
-    bool hit(const Ray& r, double tmin, double tmax, HitResult& result) const override {
+    bool hit(const Ray& r, interval t, HitResult& result) const override {
         vec3 oc = centre_ - r.origin();
         auto a = glm::dot(r.direction(), r.direction());
         auto h = glm::dot(r.direction(), oc);
@@ -61,10 +118,10 @@ public:
 
         auto sqrt_discriminant = std::sqrt(discriminant);
         auto root = (h - sqrt_discriminant) / a;
-        if (root <= tmin || root >= tmax) {
+        if (!t.surrounds(root)) {
             // Try the other root.
             root = (h + sqrt_discriminant) / a;
-            if (root <= tmin || root >= tmax) {
+            if (!t.surrounds(root)) {
                 // Both roots are outside the range.
                 return false;
             }
@@ -87,12 +144,13 @@ public:
         hittables_.emplace_back(std::move(hittable));
     }
 
-    bool hit(const Ray& r, double tmin, double tmax, HitResult& result) const override {
+    bool hit(const Ray& r, interval t, HitResult& result) const override {
         bool hitAnything = false;
+        double tmax = t.max;
 
         for (const auto& hittable : hittables_) {
             HitResult tempResult;
-            if (hittable->hit(r, tmin, tmax, tempResult)) {
+            if (hittable->hit(r, interval(t.min, tmax), tempResult)) {
                 hitAnything = true;
                 result = tempResult;
 
@@ -107,10 +165,16 @@ private:
     std::vector<std::unique_ptr<Hittable>> hittables_;
 };
 
-color ray_color(const Ray& r, const Scene& scene) {
+color rayColour(const Ray& r, int depth, const Scene& scene) {
+    if (depth == 0) {
+        // If we've run out of rays, then return no colour.
+        return color(0, 0, 0);
+    }
+
     HitResult result;
-    if (scene.hit(r, 0.0, std::numeric_limits<double>::max(), result)) {
-        return 0.5 * color(result.normal.x + 1, result.normal.y + 1, result.normal.z + 1);
+    if (scene.hit(r, interval(0.001, std::numeric_limits<double>::max()), result)) {
+        vec3 direction = result.normal + randomUnitVector();
+        return 0.5 * rayColour(Ray(result.point, direction), depth - 1, scene);
     }
     
     vec3 unitDirection = glm::normalize(r.direction());
@@ -118,15 +182,17 @@ color ray_color(const Ray& r, const Scene& scene) {
     return ((1.0 - a) * color(1.0, 1.0, 1.0)) + (a * color(0.5, 0.7, 1.0));
 }
 
-void write_color(std::vector<uint8_t>& out, const color& pixel_color) {
-    out.emplace_back(uint8_t(255 * pixel_color.x));
-    out.emplace_back(uint8_t(255 * pixel_color.y));
-    out.emplace_back(uint8_t(255 * pixel_color.z));
+void writeColour(uint8_t* data, int x, int y, int width, const color& pixel_color) {
+    int offset = (x + (y * width)) * 3;
+    data[offset] = uint8_t(255 * pixel_color.x);
+    data[offset + 1] = uint8_t(255 * pixel_color.y);
+    data[offset + 2] = uint8_t(255 * pixel_color.z);
 }
 
 int main() {
-    int image_width = 1280;
-    int image_height = 720;
+    const int imageWidth = 960;
+    const int imageHeight = 540;
+    const int maxDepth = 5;
 
     // Scene
     Scene scene;
@@ -134,39 +200,79 @@ int main() {
     scene.add(std::make_unique<Sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width)/image_height);
-    auto camera_center = point3(0, 0, 0);
+    auto focalLength = 1.0;
+    auto viewportHeight = 2.0;
+    auto viewportWidth = viewportHeight * (double(imageWidth)/imageHeight);
+    auto cameraCentre = point3(0, 0, 0);
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+    auto viewportU = vec3(viewportWidth, 0, 0);
+    auto viewportV = vec3(0, -viewportHeight, 0);
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    auto pixel_delta_u = viewport_u / double(image_width);
-    auto pixel_delta_v = viewport_v / double(image_height);
+    auto pixelDeltaU = viewportU / double(imageWidth);
+    auto pixelDeltaV = viewportV / double(imageHeight);
 
     // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center
-                             - vec3(0, 0, focal_length) - viewport_u/2.0 - viewport_v/2.0;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    auto viewportUpperLeft = cameraCentre
+                             - vec3(0, 0, focalLength) - viewportU/2.0 - viewportV/2.0;
+    auto pixelUpperLeft = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
+
+    const int samples = 10;
+
+    const int numWorkers = 16;
 
     // Render.
     std::vector<uint8_t> pixels;
-    pixels.reserve(image_width * image_height * 3);
-    for (int j = 0; j < image_height; j++) {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) {
-            auto pixel_center = pixel00_loc + (double(i) * pixel_delta_u) + (double(j) * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            write_color(pixels, ray_color(Ray{camera_center, ray_direction}, scene));
+    pixels.resize(imageWidth * imageHeight * 3);
+    uint8_t* pixelData = pixels.data();
+
+    std::atomic_int scanlineCounter(imageHeight);
+    std::vector<std::thread> workers;
+
+    for (int t = 0; t < numWorkers; ++t) {
+        // Divide the work into threads.
+        workers.emplace_back([&, t]() {
+            int numScanlines = imageHeight / numWorkers;
+            int start = numScanlines * t;
+            int end = (t == numWorkers - 1) ? imageHeight : start + numScanlines;
+            for (int j = start; j < end; j++) {
+                for (int i = 0; i < imageWidth; i++) {
+                    auto pixelCentre = pixelUpperLeft + (double(i) * pixelDeltaU) + (double(j) * pixelDeltaV);
+                    auto rayDirection = pixelCentre - cameraCentre;
+
+                    // Take the average of N samples for this pixel.
+                    color finalColour = color(0, 0, 0);
+                    for (int i = 0; i < samples; ++i) {
+                        finalColour += rayColour(Ray{cameraCentre, rayDirection}, maxDepth, scene);
+                    }
+                    finalColour /= double(samples);
+                    writeColour(pixelData, i, j, imageWidth, finalColour);
+                }
+                scanlineCounter--;
+            }
+        });
+    }
+    
+    // Kick off an additional worker thread that monitors the progress of the render.
+    workers.emplace_back([&]() {
+        int prevCounter = -1;
+        while (scanlineCounter > 0) {
+            if (scanlineCounter != prevCounter) {
+                std::clog << "\rScanlines remaining: " << scanlineCounter << ' ' << std::flush;
+            }
+            stbi_write_png("output.png", imageWidth, imageHeight, 3, pixels.data(), 0);
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
         }
+    });
+
+    for (auto& w : workers) {
+        w.join();
     }
 
-    std::clog << std::endl << "\rDone" << std::endl;
+    std::clog << std::endl << "Done" << std::endl;
 
     // Write the image to a file.
-    stbi_write_png("output.png", image_width, image_height, 3, pixels.data(), 0);
+    stbi_write_png("output.png", imageWidth, imageHeight, 3, pixels.data(), 0);
     return 0;
 }
