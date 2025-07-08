269c985d6258f8e0b84c7317c45f1dc3ee520e0b
David Avedissian <dgavedissian@github.com>
2025-07-08 20:49:45 +0100

    Display the rendered image in a window instead (using GLFW and OpenGL)

--- raytracing/raytracing.cpp
+++ raytracing/raytracing.cpp
@@ -8,8 +8,9 @@
 #include <glm.hpp>
 #include <gtc/epsilon.hpp>
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb_image_write.h>
+#define GLAD_GL_IMPLEMENTATION
+#include "glad/gl.h"
+#include <GLFW/glfw3.h>
 
 using color = glm::dvec3;
 using point3 = glm::dvec3;
@@ -261,17 +262,6 @@ private:
     std::vector<std::unique_ptr<Hittable>> hittables_;
 };
 
-double linearToGammaSpace(double value, double invGamma) {
-    return std::pow(value, invGamma);
-}
-
-void writeColour(uint8_t* data, int x, int y, int width, double invGamma, const color& pixelColour) {
-    int offset = (x + y * width) * 3;
-    data[offset] = uint8_t(255 * linearToGammaSpace(pixelColour.r, invGamma));
-    data[offset + 1] = uint8_t(255 * linearToGammaSpace(pixelColour.g, invGamma));
-    data[offset + 2] = uint8_t(255 * linearToGammaSpace(pixelColour.b, invGamma));
-}
-
 class Renderer {
 public:
     Renderer(const Scene& scene, int imageWidth, int imageHeight, int maxDepth, int samples, double gamma, point3 cameraCentre) :
@@ -283,16 +273,6 @@ public:
         invGamma_(1.0 / gamma)
     {
         setCamera(cameraCentre, point3(0, 0, -1), vec3(0, 1, 0), 90.0);
-    }
-
-    void renderPixel(uint8_t* data, int x, int y, int width) {
-        // Take the average of N samples for this pixel, with slight random offsets for anti-aliasing.
-        color pixelColour = color(0, 0, 0);
-        for (int i = 0; i < samples_; ++i) {
-            pixelColour += rayColour(getRay(x, y), maxDepth_);
-        }
-        pixelColour /= double(samples_);
-        writeColour(data, x, y, imageWidth_, invGamma_, pixelColour);
     }
 
     int imageWidth() const { return imageWidth_; }
@@ -328,6 +308,46 @@ public:
         pixelUpperLeft_ = viewportUpperLeft + 0.5 * (pixelDeltaU_ + pixelDeltaV_);
     }
 
+    void renderImage(int numWorkers, uint8_t* pixelData) {
+        int numPixels = imageWidth_ * imageHeight_;
+
+        // Spawn workers.
+        std::vector<std::thread> workers;
+        std::atomic<int> nextPixelIndex{0};
+        for (int t = 0; t < numWorkers; ++t) {
+            workers.emplace_back([&]() {
+                while (true) {
+                    auto pixelIndex = nextPixelIndex++;
+
+                    // Ran out of jobs, abort.
+                    if (pixelIndex >= numPixels) {
+                        break;
+                    }
+
+                    int x = pixelIndex % imageWidth_;
+                    int y = pixelIndex / imageWidth_;
+                    renderPixel(pixelData, x, y, imageWidth_);
+                }
+            });
+        }
+        
+        // Wait for all workers to complete.
+        for (auto& w : workers) {
+            w.join();
+        }
+    }
+
+    double linearToGammaSpace(double value, double invGamma) {
+        return std::pow(value, invGamma);
+    }
+
+    void writeColour(uint8_t* data, int x, int y, int width, double invGamma, const color& pixelColour) {
+        int offset = (x + y * width) * 3;
+        data[offset] = uint8_t(255 * linearToGammaSpace(pixelColour.r, invGamma));
+        data[offset + 1] = uint8_t(255 * linearToGammaSpace(pixelColour.g, invGamma));
+        data[offset + 2] = uint8_t(255 * linearToGammaSpace(pixelColour.b, invGamma));
+    }
+
 private:
     const Scene& scene_;
 
@@ -346,17 +366,14 @@ private:
     vec3 vup_;
     double fovDegrees_;
 
-    Ray getRay(int x, int y) {
-        // Returns the vector to a random point in the [-0.5, -0.5] - [+0.5, +0.5] unit square.
-        vec3 offset(randomDouble() - 0.5, randomDouble() - 0.5, 0);
-
-        auto pixelSample = pixelUpperLeft_ +
-            ((double(x) + offset.x) * pixelDeltaU_) +
-            ((double(y) + offset.y) * pixelDeltaV_);
-
-        auto rayDirection = pixelSample - lookfrom_;
-
-        return Ray{lookfrom_, rayDirection};
+    void renderPixel(uint8_t* data, int x, int y, int width) {
+        // Take the average of N samples for this pixel, with slight random offsets for anti-aliasing.
+        color pixelColour = color(0, 0, 0);
+        for (int i = 0; i < samples_; ++i) {
+            pixelColour += rayColour(getRay(x, y), maxDepth_);
+        }
+        pixelColour /= double(samples_);
+        writeColour(data, x, y, imageWidth_, invGamma_, pixelColour);
     }
 
     color rayColour(const Ray& r, int depth) {
@@ -379,78 +396,187 @@ private:
         auto a = 0.5 * (unitDirection.y + 1.0);
         return ((1.0 - a) * color(1.0, 1.0, 1.0)) + (a * color(0.5, 0.7, 1.0));
     }
+
+    Ray getRay(int x, int y) {
+        // Returns the vector to a random point in the [-0.5, -0.5] - [+0.5, +0.5] unit square.
+        vec3 offset(randomDouble() - 0.5, randomDouble() - 0.5, 0);
+
+        auto pixelSample = pixelUpperLeft_ +
+            ((double(x) + offset.x) * pixelDeltaU_) +
+            ((double(y) + offset.y) * pixelDeltaV_);
+
+        auto rayDirection = pixelSample - lookfrom_;
+
+        return Ray{lookfrom_, rayDirection};
+    }
 };
 
-void renderImage(Renderer& renderer, uint8_t* pixelData, int numPixels, int numWorkers, int imageWidth) {
-    // Spawn workers.
-    std::vector<std::thread> workers;
-    std::atomic<int> nextPixelIndex{0};
-    for (int t = 0; t < numWorkers; ++t) {
-        workers.emplace_back([&]() {
-            while (true) {
-                auto pixelIndex = nextPixelIndex++;
-
-                // Ran out of jobs, abort.
-                if (pixelIndex >= numPixels) {
-                    break;
-                }
-
-                int x = pixelIndex % imageWidth;
-                int y = pixelIndex / imageWidth;
-                renderer.renderPixel(pixelData, x, y, imageWidth);
-            }
-        });
-    }
-    
-    // Monitor progress.
-    auto lastWritten = std::chrono::system_clock::now();
-    const int numBars = 50;
-    while (nextPixelIndex < numPixels) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-        float progress = float(nextPixelIndex) / numPixels;
-        std::string bars = std::string(int(progress * numBars), '#') + std::string(int((1.0 - progress) * numBars), '.');
-        std::clog << "\rProgress: " << bars << " " << std::fixed << std::setprecision(1) << (progress * 100.0f) << "% " << std::flush;
-    }
-
-    // Wait for all workers to complete.
-    for (auto& w : workers) {
-        w.join();
-    }
+const char* vertexShaderSource = R"(
+#version 330 core
+layout(location = 0) in vec2 aPos;
+layout(location = 1) in vec2 aTexCoord;
+out vec2 TexCoord;
+void main() {
+    TexCoord = aTexCoord;
+    gl_Position = vec4(aPos, 0.0, 1.0);
 }
+)";
+
+const char* fragmentShaderSource = R"(
+#version 330 core
+in vec2 TexCoord;
+out vec4 FragColor;
+uniform sampler2D screenTexture;
+void main() {
+    FragColor = texture(screenTexture, TexCoord);
+}
+)";
+
+class Window {
+public:
+    Window(int width, int height, const char* title)
+        : width_(width), height_(height) {
+        if (!glfwInit()) {
+            throw std::runtime_error("Failed to initialize GLFW");
+        }
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+        window_ = glfwCreateWindow(width, height, title, nullptr, nullptr);
+        if (!window_) {
+            glfwTerminate();
+            throw std::runtime_error("Failed to create GLFW window");
+        }
+
+        glfwMakeContextCurrent(window_);
+        int version = gladLoadGL(glfwGetProcAddress);
+        if (version == 0) {
+            throw std::runtime_error("Failed to initialize OpenGL context");
+        }
+
+        // Texture
+        glGenTextures(1, &tex_);
+        glBindTexture(GL_TEXTURE_2D, tex_);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+
+        // Fullscreen quad.
+        float quadVertices[] = {
+            -1.0f,  1.0f,  0.0f, 0.0f,
+            -1.0f, -1.0f,  0.0f, 1.0f,
+             1.0f, -1.0f,  1.0f, 1.0f,
+            -1.0f,  1.0f,  0.0f, 0.0f,
+             1.0f, -1.0f,  1.0f, 1.0f,
+             1.0f,  1.0f,  1.0f, 0.0f
+        };
+        glGenVertexArrays(1, &quadVAO_);
+        glGenBuffers(1, &quadVBO_);
+        glBindVertexArray(quadVAO_);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO_);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+        // Shaders.
+        GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+        glCompileShader(vertexShader);
+        GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+        glCompileShader(fragmentShader);
+        shaderProgram_ = glCreateProgram();
+        glAttachShader(shaderProgram_, vertexShader);
+        glAttachShader(shaderProgram_, fragmentShader);
+        glLinkProgram(shaderProgram_);
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+    }
+
+    ~Window() {
+        glDeleteVertexArrays(1, &quadVAO_);
+        glDeleteBuffers(1, &quadVBO_);
+        glDeleteTextures(1, &tex_);
+        glDeleteProgram(shaderProgram_);
+        if (window_) {
+            glfwDestroyWindow(window_);
+        }
+        glfwTerminate();
+    }
+
+    bool shouldClose() const { return glfwWindowShouldClose(window_); }
+    
+    void updateTexture(const uint8_t* pixelData) {
+        glBindTexture(GL_TEXTURE_2D, tex_);
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width_, height_, GL_RGB, GL_UNSIGNED_BYTE, pixelData);
+    }
+
+    void draw() {
+        glUseProgram(shaderProgram_);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, tex_);
+        glUniform1i(glGetUniformLocation(shaderProgram_, "screenTexture"), 0);
+        glBindVertexArray(quadVAO_);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        glfwPollEvents();
+        glfwSwapBuffers(window_);
+    }
+
+private:
+    int width_, height_;
+    GLFWwindow* window_;
+    GLuint tex_;
+    GLuint quadVAO_, quadVBO_;
+    GLuint shaderProgram_;
+
+};
 
 int main() {
     const int numWorkers = std::max(1u, std::thread::hardware_concurrency() - 1);
-    const int samples = 20;
+    const int samples = 10;
 
     Scene scene;
     scene.add(std::make_unique<Sphere>(point3(0, -100.5, -1.25), 100, std::make_unique<LambertianMaterial>(color(0.5))));
-    scene.add(std::make_unique<Sphere>(point3(0, 0, -1.25), 0.5, std::make_unique<LambertianMaterial>(color(0.1, 0.2, 0.5))));
-    scene.add(std::make_unique<Sphere>(point3(1, -0.25, -1.25), 0.25, std::make_unique<GlassMaterial>(1.5)));
+    scene.add(std::make_unique<Sphere>(point3(0, 0, -1.25), 0.5, std::make_unique<GlassMaterial>(1.5)));
+    scene.add(std::make_unique<Sphere>(point3(1, -0.25, -1.25), 0.25, std::make_unique<LambertianMaterial>(color(0.1, 0.2, 0.5))));
     scene.add(std::make_unique<Sphere>(point3(-1, -0.5 + 0.4, -1.25), 0.4, std::make_unique<MetalMaterial>(color(0.8), 0.2)));
 
-    Renderer renderer(scene, 960, 540, 10, samples, 2.0, point3(0, 0, 0));
-    renderer.setCamera(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 60.0);
+    const int imageWidth = 960;
+    const int imageHeight = 540;
 
-    // Kick off rendering.
-    auto startTime = std::chrono::system_clock::now();
+    Renderer renderer(scene, imageWidth, imageHeight, 10, samples, 2.0, point3(0, 0, 0));
+    Window window(imageWidth, imageHeight, "Raytracing");
 
-    const int imageWidth = renderer.imageWidth();
-    const int imageHeight = renderer.imageHeight();
     const int numPixels = imageWidth * imageHeight;
-
-    std::vector<uint8_t> pixels;
-    pixels.resize(numPixels * 3);
+    std::vector<uint8_t> pixels(numPixels * 3);
     uint8_t* pixelData = pixels.data();
 
-    std::clog << "Rendering " << imageWidth << "x" << imageHeight << " with " << numWorkers << " workers." << std::endl;
+    auto start = std::chrono::system_clock::now();
+    while (!window.shouldClose()) {
+        auto frameStart = std::chrono::high_resolution_clock::now();
+        double elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(
+                std::chrono::system_clock::now() - start
+            ).count();
 
-    renderImage(renderer, pixels.data(), numPixels, numWorkers, imageWidth);
+        renderer.setCamera(point3(std::sin(elapsed) * 2, 1, std::cos(elapsed) * 2), point3(0, 0, -1), vec3(0, 1, 0), 60.0);
+        renderer.renderImage(numWorkers, pixelData);
+        window.updateTexture(pixelData);
 
-    auto duration = std::chrono::system_clock::now() - startTime;
-    std::clog << std::endl << "Done. Took " << std::fixed << std::setprecision(2) << std::chrono::duration_cast<std::chrono::duration<float>>(duration).count() << " seconds." << std::endl;
-
-    // Write the final image to a file.
-    stbi_write_png("output.png", imageWidth, imageHeight, 3, pixels.data(), 0);
+        window.draw();
+        
+        auto frameEnd = std::chrono::high_resolution_clock::now();
+        auto frameTimeMs = std::chrono::duration_cast<std::chrono::milliseconds>(frameEnd - frameStart).count();
+        std::cout << "\rFrame time: " << frameTimeMs << " ms" << std::flush;
+    }
     return 0;
 }
