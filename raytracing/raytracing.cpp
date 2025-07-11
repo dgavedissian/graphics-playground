ddf9a3e97381c83e3bc3a1d2ec95bd36a1a2dc08
David Avedissian <dgavedissian@github.com>
2025-07-11 18:26:23 +0100

    Made the BVHTree traversal more efficient.

--- raytracing/raytracing.cpp
+++ raytracing/raytracing.cpp
@@ -14,204 +14,12 @@
 
 #include "math.h"
 #include "material.h"
-class Hittable {
-public:
-    virtual ~Hittable() = default;
-
-    virtual bool hit(const Ray& r, interval t, HitResult& result) const = 0;
-
-    virtual AABB boundingBox() const = 0;
-};
-
-class Sphere : public Hittable {
-public:
-    Sphere(const vec3& centre, double radius, std::unique_ptr<Material> material) :
-        centre_(centre),
-        radius_(std::fmax(0.0, radius)),
-        material_(std::move(material))
-    {
-        auto rvec = vec3(radius);
-        bbox_ = AABB(centre - rvec, centre + rvec);
-    }
-
-    bool hit(const Ray& r, interval t, HitResult& result) const override {
-        vec3 oc = centre_ - r.origin();
-        auto a = glm::dot(r.direction(), r.direction());
-        auto h = glm::dot(r.direction(), oc);
-        auto c = glm::dot(oc, oc) - radius_ * radius_;
-        auto discriminant = h * h - a * c;
-        
-        if (discriminant < 0) {
-            return false;
-        }
-
-        auto sqrt_discriminant = std::sqrt(discriminant);
-        auto root = (h - sqrt_discriminant) / a;
-        if (!t.surrounds(root)) {
-            // Try the other root.
-            root = (h + sqrt_discriminant) / a;
-            if (!t.surrounds(root)) {
-                // Both roots are outside the range.
-                return false;
-            }
-        }
-
-        result.t = root;
-        result.point = r.at(root);
-        result.setNormal(r, (result.point - centre_) / radius_);
-        result.material = material_.get();
-        return true;
-    }
-
-    AABB boundingBox() const override { return bbox_; }
-
-private:
-    vec3 centre_;
-    double radius_;
-    std::unique_ptr<Material> material_;
-
-    AABB bbox_;
-
-};
-
-class BVHNode : public Hittable {
-  public:
-    BVHNode(std::vector<Hittable*>& objects, std::vector<std::unique_ptr<BVHNode>>& nodes, size_t start, size_t end) {
-        // Build the bounding box of the span of source objects.
-        bbox_ = objects[start]->boundingBox();
-        for (size_t i = start + 1; i < end; i++) {
-            bbox_ = AABB(bbox_, objects[i]->boundingBox());
-        }
-
-        int axis = bbox_.longestAxis();
-
-        auto comparator = (axis == 0) ? boxXCompare : ((axis == 1) ? boxYCompare : boxZCompare);
-        size_t objectSpan = end - start;
-
-        if (objectSpan == 1) {
-            left_ = right_ = objects[start];
-        } else if (objectSpan == 2) {
-            left_ = objects[start];
-            right_ = objects[start + 1];
-        } else {
-            std::sort(std::begin(objects) + start, std::begin(objects) + end, comparator);
-
-            auto mid = start + objectSpan / 2;
-            auto left = std::make_unique<BVHNode>(objects, nodes, start, mid);
-            auto right = std::make_unique<BVHNode>(objects, nodes, mid, end);
-            left_ = left.get();
-            right_ = right.get();
-            nodes.push_back(std::move(left));
-            nodes.push_back(std::move(right));
-        }
-    }
-
-    bool hit(const Ray& r, interval t, HitResult& result) const override {
-        if (!bbox_.hit(r, t)) {
-            return false;
-        }
-
-        bool hitLeft = left_->hit(r, t, result);
-        bool hitRight = right_->hit(r, interval(t.min, hitLeft ? result.t : t.max), result);
-
-        return hitLeft || hitRight;
-    }
-
-    AABB boundingBox() const override { return bbox_; }
-
-  private:
-    Hittable* left_;
-    Hittable* right_;
-    AABB bbox_;
-
-    static bool boxCompare(const Hittable* a, const Hittable* b, int axisIndex) {
-        auto aAxisInterval = a->boundingBox().axisInterval(axisIndex);
-        auto bAxisInterval = b->boundingBox().axisInterval(axisIndex);
-        return aAxisInterval.min < bAxisInterval.min;
-    }
-
-    static bool boxXCompare(const Hittable* a, const Hittable* b) {
-        return boxCompare(a, b, 0);
-    }
-
-    static bool boxYCompare(const Hittable* a, const Hittable* b) {
-        return boxCompare(a, b, 1);
-    }
-
-    static bool boxZCompare(const Hittable* a, const Hittable* b) {
-        return boxCompare(a, b, 2);
-    }
-};
-
-class BVHTree : public Hittable {
-public:
-    BVHTree(std::unique_ptr<BVHNode> root, std::vector<std::unique_ptr<BVHNode>> storage) :
-        root_(std::move(root)),
-        storage_(std::move(storage))
-    {
-    }
-
-    bool hit(const Ray& r, interval t, HitResult& result) const override {
-        return root_->hit(r, t, result);
-    }
-
-    AABB boundingBox() const override {
-        return root_->boundingBox();
-    }
-
-private:
-    std::unique_ptr<BVHNode> root_;
-    std::vector<std::unique_ptr<BVHNode>> storage_;
-
-};
-
-class Scene : public Hittable {
-public:
-    void add(std::unique_ptr<Hittable> hittable) {
-        bbox_ = AABB(bbox_, hittable->boundingBox());
-        hittables_.emplace_back(std::move(hittable));
-    }
-
-    bool hit(const Ray& r, interval t, HitResult& result) const override {
-        bool hitAnything = false;
-        double tmax = t.max;
-
-        for (const auto& hittable : hittables_) {
-            HitResult tempResult;
-            if (hittable->hit(r, interval(t.min, tmax), tempResult)) {
-                hitAnything = true;
-                result = tempResult;
-
-                // The maximum the ray can travel now, is to where this object was hit.
-                tmax = tempResult.t;
-            }
-        }
-        return hitAnything;
-    }
-
-    AABB boundingBox() const override { return bbox_; }
-
-    std::unique_ptr<BVHTree> generateBVHTree() const {
-        std::vector<Hittable*> hittables;
-        std::transform(
-            hittables_.begin(), hittables_.end(),
-            std::back_inserter(hittables),
-            [](const auto& p) { return p.get(); }
-        );
-
-        std::vector<std::unique_ptr<BVHNode>> nodes;
-        auto root = std::make_unique<BVHNode>(hittables, nodes, 0, hittables.size());
-        return std::make_unique<BVHTree>(std::move(root), std::move(nodes));
-    }
-
-private:
-    std::vector<std::unique_ptr<Hittable>> hittables_;
-    AABB bbox_;
-};
+#include "hittable.h"
+#include "bvh_tree.h"
 
 class Renderer {
 public:
-    Renderer(const Hittable& scene, int imageWidth, int imageHeight, int maxDepth, int samples, double gamma, vec3 cameraCentre) :
+    Renderer(const Hittable& scene, int imageWidth, int imageHeight, int maxDepth, int samples, double gamma, Vec3 cameraCentre) :
         scene_(scene),
         imageWidth_(imageWidth),
         imageHeight_(imageHeight),
@@ -219,22 +27,22 @@ public:
         samples_(samples),
         invGamma_(1.0 / gamma)
     {
-        setCamera(cameraCentre, vec3(0, 0, -1), vec3(0, 1, 0), 90.0);
+        setCamera(cameraCentre, Vec3(0, 0, -1), Vec3(0, 1, 0), 90.0);
     }
 
     int imageWidth() const { return imageWidth_; }
     int imageHeight() const { return imageHeight_; }
 
-    void setCamera(vec3 lookfrom, vec3 lookat, vec3 vup, double fovDegrees) {
+    void setCamera(Vec3 lookfrom, Vec3 lookat, Vec3 vup, double fovDegrees) {
         lookfrom_ = lookfrom;
         lookat_ = lookat;
         vup_ = vup;
         fovDegrees_ = fovDegrees;
 
         // Calculate the camera basis vectors.
-        vec3 w = glm::normalize(lookfrom_ - lookat_);
-        vec3 u = glm::normalize(glm::cross(vup_, w));
-        vec3 v = glm::cross(w, u);
+        Vec3 w = glm::normalize(lookfrom_ - lookat_);
+        Vec3 u = glm::normalize(glm::cross(vup_, w));
+        Vec3 v = glm::cross(w, u);
 
         auto focalLength = glm::length(lookfrom - lookat);
         auto theta = glm::radians(fovDegrees_);
@@ -288,7 +96,7 @@ public:
         return std::pow(value, invGamma);
     }
 
-    void writeColour(uint8_t* data, int x, int y, int width, double invGamma, const color& pixelColour) {
+    void writeColour(uint8_t* data, int x, int y, int width, double invGamma, const Vec3& pixelColour) {
         int offset = (x + y * width) * 3;
         data[offset] = uint8_t(255 * linearToGammaSpace(glm::clamp(pixelColour.r, 0.0, 1.0), invGamma));
         data[offset + 1] = uint8_t(255 * linearToGammaSpace(glm::clamp(pixelColour.g, 0.0, 1.0), invGamma));
@@ -304,18 +112,18 @@ private:
     int samples_;
     double invGamma_;
 
-    vec3 pixelUpperLeft_;
-    vec3 pixelDeltaU_;
-    vec3 pixelDeltaV_;
+    Vec3 pixelUpperLeft_;
+    Vec3 pixelDeltaU_;
+    Vec3 pixelDeltaV_;
     
-    vec3 lookfrom_;
-    vec3 lookat_;
-    vec3 vup_;
+    Vec3 lookfrom_;
+    Vec3 lookat_;
+    Vec3 vup_;
     double fovDegrees_;
 
     void renderPixel(uint8_t* data, int x, int y, int width) {
         // Take the average of N samples for this pixel, with slight random offsets for anti-aliasing.
-        color pixelColour = color(0, 0, 0);
+        Vec3 pixelColour = Vec3(0, 0, 0);
         for (int i = 0; i < samples_; ++i) {
             pixelColour += rayColour(getRay(x, y), maxDepth_);
         }
@@ -323,27 +131,23 @@ private:
         writeColour(data, x, y, imageWidth_, invGamma_, pixelColour);
     }
 
-    color rayColour(const Ray& r, int depth) {
+    Vec3 rayColour(const Ray& r, int depth) {
         if (depth == 0) {
             // If we've run out of rays, then return no colour.
-            return color(0, 0, 0);
+            return Vec3(0, 0, 0);
         }
 
         // Perform ray test.
         HitResult result;
-        bool hit = scene_.hit(r, interval(0.001, std::numeric_limits<double>::max()), result);
+        bool hit = scene_.hit(r, Interval(0.001, std::numeric_limits<double>::max()), result);
 
         if (!hit) {
-            return color(0.05, 0.1, 0.2);
-            // Return the background colour.
-            // vec3 unitDirection = glm::normalize(r.direction());
-            // auto a = 0.5 * (unitDirection.y + 1.0);
-            // return ((1.0 - a) * color(1.0, 1.0, 1.0)) + (a * color(0.5, 0.7, 1.0));
+            return Vec3(0.4, 0.6, 0.9);
         }
 
-        Ray scattered{vec3(0.0), vec3(0.0)};
-        color attenuation;
-        color emission = result.material->emitted(result.point);
+        Ray scattered{Vec3(0.0), Vec3(0.0)};
+        Vec3 attenuation;
+        Vec3 emission = result.material->emitted(result.point);
 
         if (result.material->scatter(r, result, attenuation, scattered)) {
             return emission + attenuation * rayColour(scattered, depth-1);
@@ -353,7 +157,7 @@ private:
 
     Ray getRay(int x, int y) {
         // Returns the vector to a random point in the [-0.5, -0.5] - [+0.5, +0.5] unit square.
-        vec3 offset(randomDouble() - 0.5, randomDouble() - 0.5, 0);
+        Vec3 offset(randomDouble() - 0.5, randomDouble() - 0.5, 0);
 
         auto pixelSample = pixelUpperLeft_ +
             ((double(x) + offset.x) * pixelDeltaU_) +
@@ -500,16 +304,16 @@ int main() {
     const int samples = 10;
 
     Scene scene;
-    scene.add(std::make_unique<Sphere>(vec3(0, -1000, 0), 1000, std::make_unique<LambertianMaterial>(color(0.5))));
-    // scene.add(std::make_unique<Sphere>(vec3(0, 0, -1.25), 0.5, std::make_unique<GlassMaterial>(1.5)));
-    // scene.add(std::make_unique<Sphere>(vec3(1, -0.25, -1.25), 0.25, std::make_unique<LambertianMaterial>(color(0.1, 0.2, 0.5))));
-    // scene.add(std::make_unique<Sphere>(vec3(-1, -0.5 + 0.4, -1.25), 0.4, std::make_unique<MetalMaterial>(color(0.8), 0.2)));
+    scene.add(std::make_unique<Sphere>(Vec3(0, -1000, 0), 1000, std::make_unique<LambertianMaterial>(Vec3(0.5))));
+    // scene.add(std::make_unique<Sphere>(Vec3(0, 0, -1.25), 0.5, std::make_unique<GlassMaterial>(1.5)));
+    // scene.add(std::make_unique<Sphere>(Vec3(1, -0.25, -1.25), 0.25, std::make_unique<LambertianMaterial>(Vec3(0.1, 0.2, 0.5))));
+    // scene.add(std::make_unique<Sphere>(Vec3(-1, -0.5 + 0.4, -1.25), 0.4, std::make_unique<MetalMaterial>(Vec3(0.8), 0.2)));
 
     const int size = 10;
     for (int x = -size; x <= size; ++x) {
         for (int y = -size; y <= size; ++y) {
             auto size = randomDouble(0.15, 0.3);
-            vec3 centre{x + 0.9 * randomDouble(), size, y + 0.9 * randomDouble()};
+            Vec3 centre{x + 0.9 * randomDouble(), size, y + 0.9 * randomDouble()};
 
             std::unique_ptr<Material> material;
             switch (randomInt(0, 3)) {
@@ -531,14 +335,13 @@ int main() {
         }
     }
 
-    scene.add(std::make_unique<Sphere>(vec3(3, 5, 0), 2, std::make_unique<LightMaterial>(color(3))));
-
-    auto bvh_tree = scene.generateBVHTree();
+    scene.add(std::make_unique<Sphere>(Vec3(3, 5, 0), 2, std::make_unique<LightMaterial>(Vec3(3))));
 
     const int imageWidth = 960;
     const int imageHeight = 540;
 
-    Renderer renderer(*bvh_tree, imageWidth, imageHeight, 10, samples, 2.0, vec3(0, 0, 0));
+    auto bvhTree = generateBVHTree(scene);
+    Renderer renderer(*bvhTree, imageWidth, imageHeight, 10, samples, 2.0, Vec3(0, 0, 0));
     Window window(imageWidth, imageHeight, "Raytracing");
 
     const int numPixels = imageWidth * imageHeight;
@@ -552,7 +355,7 @@ int main() {
                 std::chrono::system_clock::now() - start
             ).count();
 
-        renderer.setCamera(vec3(std::sin(elapsed * 0.5) * 4, 1.5, std::cos(elapsed * 0.5) * 4 - 1.0), vec3(0, 0.5, -1), vec3(0, 1, 0), 60.0);
+        renderer.setCamera(Vec3(std::sin(elapsed * 0.5) * 4, 1.5, std::cos(elapsed * 0.5) * 4 - 1.0), Vec3(0, 0.5, -1), Vec3(0, 1, 0), 60.0);
         renderer.renderImage(numWorkers, pixelData);
         window.updateTexture(pixelData);
 
